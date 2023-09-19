f9581b46a43568c999498a31ebbeeabbee444de6
Noah Salmon <noahsalmon98@gmail.com>
2023-09-19 15:23:56 -0400

    working on homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+const AppHome = () => {
+  // Sample data for projects
+  const projects = [
+    {
+      title: 'Name1',
+      description: 'Description',
+      image: '',
+      link: 'Link',
+    },
+    {
+        title: 'Name2',
+        description: 'Description',
+        image: '',
+        link: 'Link',
+    },
+    {
+        title: 'Name3',
+        description: 'Description',
+        image: '',
+        link: 'Link',
+    },
+  ];
+
+  return (
+ <div>Hello</div>
+  )}
+
+export default AppHome;
