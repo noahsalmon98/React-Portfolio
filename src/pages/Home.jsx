8412d71568ab26f7a0042dcf6ec68b4efea604bf
Noah Salmon <noahsalmon98@gmail.com>
2023-09-19 15:27:06 -0400

    working on homepage - added projects section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,6 +38,23 @@ const AppHome = () => {
         </p>
       </section>
 
+      <section className="projects">
+        <h2>Projects</h2>
+        <div className="project-list">
+          {projects.map((project, index) => (
+            <div className="project" key={index}>
+              <img src={project.image} alt={project.title} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <a href={project.link} target="_blank" rel="noopener noreferrer">
+                View Project
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+
+
     </div>
   );
 };
